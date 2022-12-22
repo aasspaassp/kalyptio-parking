b3c8329d35bc3b7d7f6eb999be7608b76ee8f5ed
aasspaassp <andresfilos94@gmail.com>
2022-12-22 17:00:58 -0600

    DEPENDENCIES INSTALLES AND INDEX IS CREATED

---
+++ Backend/src/app.ts
@@ -0,0 +1,8 @@
+import "dotenv/config";
+import express from "express"
+import cors from "cors"
+const PORT = process.env.PORT || 3033;
+const app = express();
+
+app.use(cors())
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
